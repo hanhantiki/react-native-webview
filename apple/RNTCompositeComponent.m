934299b817237cf54ddb73a62a79a006584faeb4
Viet Nguyen <ho@ngviet.com>
2021-10-03 20:05:13 +0700

    adds composite component

---
+++ apple/RNTCompositeComponent.m
@@ -0,0 +1,37 @@
+//
+//  RNTCompositeComponent.m
+//  TikiMiniApp
+//
+//  Created by Viet Nguyen on 30/09/2021.
+//
+
+#import "RNTCompositeComponent.h"
+#import <React/RCTBridge.h>
+#import <React/RCTUIManager.h>
+#import <React/UIView+React.h>
+#import <react-native-webview/RNCWebView.h>
+
+@implementation RNTCompositeComponent {
+  __weak RCTBridge *_bridge;
+}
+
+RCT_NOT_IMPLEMENTED(-(instancetype)initWithFrame : (CGRect)frame)
+RCT_NOT_IMPLEMENTED(-(instancetype)initWithCoder : coder)
+
+- (instancetype)initWithBridge:(RCTBridge *)bridge {
+  if ((self = [super initWithFrame:CGRectZero])) {
+     _bridge = bridge;
+   }
+   return self;
+}
+
+- (void)didUpdateReactSubviews {
+  RCTUIManager* uiManager = [_bridge moduleForClass:[RCTUIManager class]];
+  RNCWebView* webView = (RNCWebView*)[uiManager viewForReactTag:self.webviewTag];
+  for (NSUInteger i = 0; i < self.reactSubviews.count; i++) {
+    UIView *subview = [self.reactSubviews objectAtIndex:i];
+    [webView addNativeComponent:subview];
+  }
+}
+
+@end
